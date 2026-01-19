c1aee24c6a088c19e9420e80b76495f931029de6
gpego1 <gpego786@gmail.com>
2026-01-18 23:48:54 -0300

    feat Dockerfile

---
+++ server/primegarage/src/main/java/io/github/gpego1/primegarage/PrimegarageApplication.java
@@ -0,0 +1,14 @@
+package io.github.gpego1.primegarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimegarageApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(PrimegarageApplication.class, args);
+        System.out.println("Hello docker world!");
+    }
+
+}
---
+++ server/primegarage/src/test/java/io/github/gpego1/primegarage/PrimegarageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gpego1.primegarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimegarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
